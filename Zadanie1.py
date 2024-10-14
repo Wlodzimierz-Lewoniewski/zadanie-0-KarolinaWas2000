559bca95d5d08e94ed756db5ac208f0df723bf41
KarolinaWas2000 <77406466+KarolinaWas2000@users.noreply.github.com>
2024-10-14 09:03:33 +0200

    Add files via upload
    Zadanie 1 ver 1

---
+++ Zadanie1.py
@@ -0,0 +1,37 @@
+# Pobranie liczby zdań
+liczba_zdan = int(input("Podaj liczbę zdań: "))
+
+# Pobranie zdań
+zdania = []
+for _ in range(liczba_zdan):
+    zdanie = input("Podaj zdanie: ")
+    zdania.append(zdanie)
+
+# Pobranie liczby słów do sprawdzenia
+liczba_slow = int(input("Podaj liczbę słów: "))
+
+# Pobranie słów do sprawdzenia
+slowa_do_policzenia = []
+for _ in range(liczba_slow):
+    slowo = input("Podaj słowo do policzenia: ")
+    slowa_do_policzenia.append(slowo)
+
+# Funkcja do liczenia i sortowania zdań według wystąpień słowa
+def sortuj_zdania(slowo, zdania):
+    licznik_slow = []
+    for i, zdanie in enumerate(zdania):
+        count = zdanie.lower().split().count(slowo.lower())
+        if count > 0:
+            licznik_slow.append((i, count))
+
+    def sortowanie(pary):
+        return (-pary[1], pary[0])  # Sortowanie według liczby wystąpień malejąco, a potem według indeksu rosnąco
+
+    licznik_slow.sort(key=sortowanie)
+
+    # Zwracanie indeksów zdań, tylko jeżeli słowo wystąpiło w zdaniu
+    return [indeks for indeks, _ in licznik_slow]
+
+for slowo in slowa_do_policzenia:
+    wynik = sortuj_zdania(slowo, zdania)
+    print(wynik)
